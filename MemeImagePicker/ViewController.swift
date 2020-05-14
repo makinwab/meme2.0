6858c7691e35ef369e8c870077b30696a63a1eae
Makinwa Olubukola <makinwa37@gmail.com>
2020-05-14 17:49:14 +0200

    Initial Commit

---
+++ MemeImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemeImagePicker
+//
+//  Created by Olubukola Makinwa on 14.05.20.
+//  Copyright © 2020 Makinwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
